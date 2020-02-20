62d3ccf8e828f8154c38170f4eade66d97b2d9b4
Ruslan Farkhutdinov <fr3ddy4@yandex.ru>
2020-02-20 22:22:39 +0300

    Basic project set up

---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const tsImportPluginFactory = require("ts-import-plugin");
+
+module.exports = (env, argv) => {
+  const outputDir = path.join(__dirname, "public");
+  const srcDir = path.join(__dirname, "components");
+
+  return {
+    target: "web",
+    entry: "./frontend/index.ts",
+    output: {
+      path: path.join(outputDir),
+      filename: path.join("js", "bundle.js")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+          options: {
+            getCustomTransformers: () => ({
+              before: [tsImportPluginFactory({ style: "css" })]
+            })
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                modules: true,
+                localIdentName: "[local]___[hash:base64:5]",
+              }
+            },
+            {
+              loader: "less-loader"
+            }
+          ]
+        },
+        {
+          test: /\.(gif|png|jpg|svg)$/,
+          use: "file-loader"
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          use: ["url-loader?limit=100000"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: path.join("css", "styles.css")
+      })
+    ]
+  };
+};
---
+++ webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const common = require("./webpack.common.js");
+
+module.exports = (env, argv) => {
+  const outputDir = path.join(__dirname, "public");
+  const srcDir = path.join(__dirname, "src");
+  const restProxyTarget =
+    (env && env.restProxyTarget) || "https://xn--b1add.xn--80aaa3bi1bsw.xn--p1ai/";
+
+  console.log(restProxyTarget)
+
+  return merge(common(env, argv), {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+      hot: true,
+      open: false,
+      contentBase: outputDir,
+      publicPath: "/",
+      historyApiFallback: true,
+      proxy: [
+        {
+          context: ["/api/"],
+          target: restProxyTarget
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          production: false
+        }
+      }),
+      new webpack.SourceMapDevToolPlugin({
+        filename: "[file].map",
+        moduleFilenameTemplate: path.relative(srcDir, "[resourcePath]")
+      })
+    ]
+  });
+};
