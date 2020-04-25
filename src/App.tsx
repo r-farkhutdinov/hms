dc0292c91af22b76cfcfd83538062295a8e71577
Ruslan Farkhutdinov <rfarkhutdinov@innodata.ru>
2020-04-25 17:16:25 +0300

    Apollo provider added

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,20 @@ import { IntlProvider } from "react-intl";
 import { useStore } from "./core/util";
 import { messages } from "./i18n";
 import { observer } from "mobx-react";
+import { client } from "../src/core/apollo";
+import { ApolloProvider } from "@apollo/client";
 
 export const App: React.FC = observer(() => {
   const { i18n } = useStore();
   const { locale } = i18n;
 
   return (
-    <IntlProvider locale={locale} messages={messages[locale]}>
-      <BrowserRouter>
-        <MainLayout />
-      </BrowserRouter>
-    </IntlProvider>
+    <ApolloProvider client={client()}>
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <BrowserRouter>
+          <MainLayout />
+        </BrowserRouter>
+      </IntlProvider>
+    </ApolloProvider>
   );
 });
